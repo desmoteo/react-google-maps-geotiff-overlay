402337d0edc7c0f0643fa01e97d51204c4324db0
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2019-10-15 17:03:13 +0200

    Fixed lifecycle

--- example/src/App.js
+++ example/src/App.js
@@ -1,65 +1,87 @@
 import React, { Component } from 'react'
 
+import ReactTimeout from 'react-timeout'
+
 import GeoTIFFOverlay from 'react-google-maps-geotiff-overlay'
 
-import { Input, Container, Button } from 'semantic-ui-react'
+import { Input, Container, Segment, Button } from 'semantic-ui-react'
 
-import { GoogleMap, LoadScript } from "@react-google-maps/api"
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
 
 import './App.css'
 
 class App extends Component {
-
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = { overlayData: null, apiKey: null, apiKeyInput: null }
+  }
 
+  fetcher = async (path) => {
+    const response = await fetch(path)
+    const blob = await response.blob()
+    const array = await blob.arrayBuffer()
+    this.setState({ overlayData: array })
+  }
+
+  timer1 = async () => {
+    await this.fetcher('prova-render.tif')
+  }
+
+  timer2 = async () => {
+    await this.fetcher('welcome.tif')
   }
 
   async componentDidMount() {
 
-    const response = await fetch('prova-render.tif');
-    const blob = await response.blob();
-    const array = await blob.arrayBuffer();
-    this.setState({ overlayData: array })
-
   }
 
-  shouldComponentUpdate(nextProps, nextState)
-  {
-    const res = nextProps !== this.props || nextState.apiKey !== this.state.apiKey
+  shouldComponentUpdate(nextProps, nextState) {
+    const res = nextProps !== this.props || nextState.apiKey !== this.state.apiKey || nextState.overlayData !== this.state.overlayData
     return (res)
   }
 
   drawMap() {
     if (this.state.apiKey !== null) {
       return (
-        <LoadScript
-          id="script-loader"
-          googleMapsApiKey={this.state.apiKey}
-        >
-          <GoogleMap
-            onLoad={map => {
-              this.mapRef = map
-            }}
-            id="circle-example"
-            mapContainerStyle={{
-              height: "768px",
-              width: "100%"
-            }}
-            zoom={4}
-            center={{ lat: 30.397, lng: 10.644 }}
+        <div>
+          <Segment raised>
+            <Container textAlign='center' style={{ padding: '30px' }} >
+              <p>
+                Example will load 1 GeoTiff after 10 seconds, and a second one after 20 seconds
+              </p>
+            </Container>
+          </Segment>
+          <LoadScript
+            id='script-loader'
+            googleMapsApiKey={this.state.apiKey}
           >
-            <GeoTIFFOverlay overlayData={this.state.overlayData} draw ></GeoTIFFOverlay>
-          </GoogleMap>
-        </LoadScript >)
+            <GoogleMap
+              onLoad={map => {
+                this.mapRef = map
+              }}
+              id='circle-example'
+              mapContainerStyle={{
+                height: '768px',
+                width: '100%'
+              }}
+              zoom={4}
+              center={{ lat: 30.397, lng: 10.644 }}
+            >
+              <GeoTIFFOverlay overlayData={this.state.overlayData} draw opacity={1} />
+            </GoogleMap>
+          </LoadScript >
+        </div>
+      )
+    } else {
+      return (
+        <Segment raised>
+          <Container textAlign='center' style={{ padding: '30px' }} >
+            <Input placeholder='Google API Key ...' onChange={this.onChangeApiKey} /><Button onClick={this.onButtonClick}>Go!</Button>
+          </Container>
+        </Segment>
+      )
     }
-    else {
-      return null
-    }
-
-
   }
 
   onChangeApiKey = (event, data) => {
@@ -67,16 +89,14 @@ class App extends Component {
   }
 
   onButtonClick = () => {
+    this.props.setTimeout(this.timer1, 10000)
+    this.props.setTimeout(this.timer2, 20000)
     this.setState({ apiKey: this.state.apiKeyInput })
   }
-
 
   render() {
     return (
       <div >
-        <Container textAlign='center' style={{ padding: "30px" }} >
-          <Input placeholder='API Key ...' onChange={this.onChangeApiKey} /><Button onClick={this.onButtonClick}>Go!</Button>
-        </Container>
         <div className='map-wrapper'>
           {this.drawMap()}
         </div>
@@ -84,7 +104,6 @@ class App extends Component {
 
     )
   }
-
 }
 
-export default App
+export default ReactTimeout(App)
--- src/index.js
+++ src/index.js
@@ -4,136 +4,136 @@ import {
   OverlayView
 } from '@react-google-maps/api'
 
-const GeoTIFF = require('geotiff');
+const GeoTIFF = require('geotiff')
+class GeoTIFFOverlay extends Component {
 
-
-export default class GeoTIFFOverlay extends Component {
   static propTypes = {
-    text: PropTypes.string
+    draw: PropTypes.bool,
+    overlayData: PropTypes.object,
+    opacity: PropTypes.number
+  }
+
+  static defaultprops = {
+    draw: true,
+    opacity: 0.5
   }
 
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.canvasRef = null;
+    this.canvasRef = null
+    this.raster = null
 
-    this.state = { showOverlay: true, canvasRef: false };
+    this.canvasWidth = 0
+    this.canvasHeight = 0
 
-    this.setCanvasRef = element => {
-      this.setState()
-      this.canvasRef = element;
-      this.setState({ canvasRef: false });
-    };
+    this.state = {
+      showOverlay: true,
+      bounds: new window.google.maps.LatLngBounds(
+        new window.google.maps.LatLng(0, 0),
+        new window.google.maps.LatLng(0, 0)
+      )
+    }
+
+    this.loadRaster(this.props).then()
   }
 
-
-
-
-  
-
-  setupListener () {
-
+  setCanvasRef = element => {
+    this.canvasRef = element
+    this.setCanvas()
   }
 
-
-async setCanvas() {
-
-  var canvas = this.canvasRef;
-  if (canvas) {
-    if (this.state.showOverlay) {
-      if (this.props.overlayData) {
-
-        const gtiff = await GeoTIFF.fromArrayBuffer(this.props.overlayData);
-        const image = await gtiff.getImage();
-        const raster = await image.readRGB({ enableAlpha: true });
-        if (this.state.showOverlay) {
-          canvas.width = image.getWidth();
-          canvas.height = image.getHeight();
-          var ctx = canvas.getContext("2d");
-          var imageData = ctx.createImageData(canvas.width, canvas.height);
-
-          var data = imageData.data;
-          var o = 0;
-          for (var i = 0; i < raster.length; i += 4) {
-            data[o] = raster[i];
-            data[o + 1] = raster[i + 1];
-            data[o + 2] = raster[i + 2];
-            data[o + 3] = raster[i + 3]; //255;//(raster[i] + raster[i + 1] + raster[i + 2]) / 3;
-            o += 4;
-          }
-          ctx.putImageData(imageData, 0, 0);
-        }
-        else {
-          const context = canvas.getContext('2d');
-          context.clearRect(0, 0, canvas.width, canvas.height);
-        }
-      }
+  async loadRaster(p) {
+    if (p.overlayData) {
+      const gtiff = await GeoTIFF.fromArrayBuffer(p.overlayData)
+      const image = await gtiff.getImage()
+      this.canvasWidth = image.getWidth()
+      this.canvasHeight = image.getHeight()
+      const bbox = image.getBoundingBox()
+      var bounds = new window.google.maps.LatLngBounds(
+        new window.google.maps.LatLng(bbox[1], bbox[0]),
+        new window.google.maps.LatLng(bbox[3], bbox[2])
+      )
+      this.raster = await image.readRGB({ enableAlpha: true })
+      this.setState({ bounds: bounds })
     }
   }
 
-}
+  setCanvas() {
+    if (this.props.draw && this.props.overlayData !== null) {
+      var canvas = this.canvasRef
+      canvas.width = this.canvasWidth
+      canvas.height = this.canvasHeight
 
+      var ctx = canvas.getContext('2d')
+      var imageData = ctx.createImageData(canvas.width, canvas.height)
 
-async componentDidUpdate(prevProps) {
-  if (this.props.draw) { }
-  await this.setCanvas();
-}
-
-async componentDidMount() {
-  await this.setCanvas();
-}
-
-
-drawCanvas() {
-  if (this.props.draw) {
-    return (<canvas style={{ position: 'absolute', height: "100%", width: "100%" }} ref={this.setCanvasRef} ></canvas>)
+      var data = imageData.data
+      var o = 0
+      for (var i = 0; i < this.raster.length; i += 4) {
+        data[o] = this.raster[i]
+        data[o + 1] = this.raster[i + 1]
+        data[o + 2] = this.raster[i + 2]
+        data[o + 3] = this.raster[i + 3] // 255;//(raster[i] + raster[i + 1] + raster[i + 2]) / 3;
+        o += 4
+      }
+      ctx.putImageData(imageData, 0, 0)
+    } else {
+      var canvas = this.canvasRef
+      const context = canvas.getContext('2d')
+      context.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+    }
   }
-  else {
-    return (null)
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.overlayData !== this.props.overlayData) {
+      this.loadRaster(nextProps)
+      return false
+    }
+    return true
+  }
+
+  async componentDidUpdate(prevProps) {
+    this.setCanvas()
+  }
+
+  async componentDidMount() {
+  }
+
+  drawCanvas() {
+    if (this.props.draw) {
+      return (<canvas style={{ position: 'absolute', height: '100%', width: '100%', opacity: this.props.opacity }} ref={this.setCanvasRef} />)
+    } else {
+      return (null)
+    }
+  }
+
+  render() {
+    return (
+      <OverlayView
+        /*
+         * An alternative to specifying position is specifying bounds.
+         * bounds can either be an instance of google.maps.LatLngBounds
+         * or an object in the following format:
+         * bounds={{
+         *    ne: { lat: 62.400471, lng: -150.005608 },
+         *    sw: { lat: 62.281819, lng: -150.287132 }
+         * }}
+         */
+        bounds={this.state.bounds}
+        /*
+         * 1. Specify the pane the OverlayView will be rendered to. For
+         *    mouse interactivity, use `OverlayView.OVERLAY_MOUSE_TARGET`.
+         *    Defaults to `OverlayView.OVERLAY_LAYER`.
+         */
+        mapPaneName={OverlayView.OVERLAY_LAYER}
+      >
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }} >
+          {this.drawCanvas()}
+        </div>
+      </OverlayView>
+    )
   }
 }
 
-render() {
-  const {
-    text
-  } = this.props
-
-  return (
-    <OverlayView
-      //position={{ lat: -34.397, lng: 150.644 }}
-      /*
-       * An alternative to specifying position is specifying bounds.
-       * bounds can either be an instance of google.maps.LatLngBounds
-       * or an object in the following format:
-       * bounds={{
-       *    ne: { lat: 62.400471, lng: -150.005608 },
-       *    sw: { lat: 62.281819, lng: -150.287132 }
-       * }}
-       */
-      bounds={new window.google.maps.LatLngBounds(
-        new window.google.maps.LatLng(36.576281573, 13.968033468),
-        new window.google.maps.LatLng(38.42004747, 15.706401162)
-      )}
-      /*
-       * 1. Specify the pane the OverlayView will be rendered to. For
-       *    mouse interactivity, use `OverlayView.OVERLAY_MOUSE_TARGET`.
-       *    Defaults to `OverlayView.OVERLAY_LAYER`.
-       */
-      mapPaneName={OverlayView.OVERLAY_LAYER}
-    /*
-     * 2. Tweak the OverlayView's pixel position. In this case, we're
-     *    centering the content.
-     */
-    //getPixelPositionOffset={this.getPixelPositionOffset}
-    /*
-     * 3. Create OverlayView content using standard React components.
-     */
-    >
-      <div style={{ position: 'absolute', height: "100%", width: "100%" }}>
-        {this.drawCanvas()}
-      </div>
-    </OverlayView>
-  )
-}
-
-}
+export default GeoTIFFOverlay
